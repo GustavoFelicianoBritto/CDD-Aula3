58596788987cc523e86c49e4a0613f5c67c8dcbb
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-06-12 14:45:38 -0300

    Exericio 3 for

--- src/javaAula3/Exercicio1.java
+++ src/javaAula3/Exercicio1.java
@@ -1,5 +1,35 @@
 package javaAula3;
+import java.util.Scanner;
 
 public class Exercicio1 {
-
+	
+	
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		float soma=0;
+		int notasInseridas=0;
+		float notaTemp;
+		System.out.print("Digite a quantidade de alunos na turma: ");
+		int resp = input.nextInt();
+		
+		while(notasInseridas<resp)
+		{
+			System.out.printf("Digite a nota do aluno %d: ",notasInseridas+1);
+			notaTemp= input.nextFloat();
+			soma=soma+notaTemp;
+			notasInseridas++;
+			
+		}
+		float media=soma/resp;
+		System.out.printf("A média dos %d alunos foi %.1f",resp,media);
+		
+		input.close();
+		
+		
+		
+	}
+	
+	
+	
+	
 }
